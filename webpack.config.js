60c4854ed991ec4f93c0b1b5393e2d4cc6d28d8a
Rashid <raabdulk@gmail.com>
2023-08-12 18:51:19 +0300

    Добавил const CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const mode =
     process.env.NODE_ENV === 'production' ? 'production' : 'development';
